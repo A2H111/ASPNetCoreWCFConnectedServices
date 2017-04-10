7811c64e7a7489294d3ab26c598e5ce92f116a29
A2H <ajeesh.varghese@hotmail.com>
2017-04-09 23:01:22 -0400

    WCFConnectedServices
    This is a demo application on how to consume WCF Service in Asp.Net Core
    Web Application using ConnectedServices.

---
+++ Client/WCFClientWebApp/WCFClientWebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleServiceReference;
+using System.Threading;
+
+namespace WCFClientWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            //Creating Client object of Service Reference
+            SimpleServiceReference.SimpleServiceClient client = new SimpleServiceReference.SimpleServiceClient();
+            //Call the method from WCF Service
+            string result = await client.GetDataAsync(1);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
